9b9affc6adcaac5979c0dc71dc368525ad134f5d
Jack Roseman <jackrose@seas.upenn.edu>
2019-04-04 05:12:23 +0000

    new file:   asm_parser.c 	new file:   assembler.c update

---
+++ asm_parser.c
@@ -0,0 +1,418 @@
+/***************************************************************************
+ * file name   : asm_parser.c                                              *
+ * author      : tjf & jack roseman                                        *
+ * description : This program will parse a .ASM file                       *
+ *                  						                               * 
+ *                                                                         *
+ ***************************************************************************
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "asm_parser.h"
+
+int read_asm_file(char* filename, char program[ROWS][COLS]) {
+	char line[COLS];
+	int i = 0;
+	FILE* file = fopen(filename, "r");
+	if (file == NULL) {
+		printf("error2: read_asm_file() failed.\n");
+		return 2;
+	}
+	while (fgets(line, COLS, file) != NULL) {
+		if (line[0] == '\n') {
+			i++;
+			continue;
+		}
+		line[strlen(line) - 1] = '\0';    //replace new line with null
+		memcpy(program[i], line, strlen(line)); 
+		i++;
+	}
+	
+	return 0;
+}
+int parse_instruction(char* instr, char* instr_bin_str) {
+	char* token;
+	char instruction[strlen(instr)];
+    const char space_delim[2] = " ";
+	memcpy(instruction, instr, strlen(instr));
+	token = strtok(instruction, space_delim);
+	if (strncmp(token, "ADD", 4) == 0) {
+		if (parse_add(instr, instr_bin_str) == 4) {
+			printf(" error4: parse_add() failed.\n");
+			return 3;
+		}
+	} else if (strncmp(token, "SUB", 4) == 0) {
+		if (parse_sub(instr, instr_bin_str) == 4) {
+			printf(" error4: parse_sub() failed.\n");
+			return 3;
+		}
+	} else if (strncmp(token, "MUL", 4) == 0) {
+		if (parse_add(instr, instr_bin_str) == 4) {
+			printf(" error4: parse_mul() failed.\n");
+			return 3;
+		}
+	} else if (strncmp(token, "DIV", 4) == 0) {
+		if (parse_add(instr, instr_bin_str) == 4) {
+			printf(" error4: parse_div() failed.\n");
+			return 3;
+		}
+	} else if (strncmp(token, "AND", 4) == 0) {
+		if (parse_add(instr, instr_bin_str) == 4) {
+			printf(" error4: parse_and() failed.\n");
+			return 3;
+		}
+	} else if (strncmp(token, "OR", 3) == 0) {
+		if (parse_or(instr, instr_bin_str) == 4) {
+			printf(" error4: parse_or() failed.\n");
+			return 3;
+		}
+	} else if(strncmp(token, "XOR", 4) == 0) {
+		if (parse_add(instr, instr_bin_str) == 4) {
+			printf(" error4: parse_xor() failed.\n");
+			return 3;
+		}
+	}
+    printf("%s\n", instr_bin_str);
+	return 0;
+}
+
+int parse_reg(char reg_num, char* instr_bin_str) {
+	int i;
+	i = strlen(instr_bin_str);        //where the number in instr_bin_str will be put
+	switch (reg_num) {
+		case '0':
+			instr_bin_str[i]   = '0';
+			instr_bin_str[i+1] = '0';
+			instr_bin_str[i+2] = '0';
+			break;
+		case '1':
+			instr_bin_str[i]   = '0';
+			instr_bin_str[i+1] = '0';
+			instr_bin_str[i+2] = '1';
+			break;
+		case '2':
+			instr_bin_str[i]   = '0';
+			instr_bin_str[i+1] = '1';
+			instr_bin_str[i+2] = '0';
+			break;
+		case '3':
+			instr_bin_str[i]   = '0';
+			instr_bin_str[i+1] = '1';
+			instr_bin_str[i+2] = '1';
+			break;
+		case '4':
+			instr_bin_str[i]   = '1';
+			instr_bin_str[i+1] = '0';
+			instr_bin_str[i+2] = '0';
+			break;
+		case '5':
+			instr_bin_str[i]   = '1';
+			instr_bin_str[i+1] = '0';
+			instr_bin_str[i+2] = '1';
+			break;
+		case '6':
+			instr_bin_str[i]   = '1';
+			instr_bin_str[i+1] = '1';
+			instr_bin_str[i+2] = '0';
+			break;
+		case '7':
+			instr_bin_str[i]   = '1';
+			instr_bin_str[i+1] = '1';
+			instr_bin_str[i+2] = '1';
+		default:
+			printf("error5: parse_reg() failed.\n");
+			return 5;
+	}
+    
+	return 0;
+}
+
+int parse_add(char* instr, char* instr_bin_str ){
+	char registers[3];
+	char* token; 
+    char r;
+	int i = 0;
+	const char space_delim[2] = " ";
+	const char null_delim[1] = "";
+	const char comma_delim[2] = ", ";
+	token = strtok(instr, space_delim);          //skip command
+    instr = instr + strlen(token) + 1;           //go to next token
+    token = strtok(NULL, comma_delim);
+	while (token != NULL) {
+        sscanf(token, "R%c", &r);
+		registers[i] = '\0' + r;
+		if (i == 2) {
+			token = strtok(NULL, null_delim);
+		} else{
+			token = strtok(NULL, comma_delim);
+		}
+		i++;
+	}
+    memcpy(instr_bin_str, "0001", 5); //opcode
+	if (parse_reg(registers[0], instr_bin_str) == 5) {
+		printf("error4: parse_add() failed because parse_reg failed.\n");
+		return 4;
+	}
+	if (parse_reg(registers[1], instr_bin_str) == 5) {
+		printf("error4: parse_add() failed because parse_reg failed.\n");
+		return 4;
+	}
+	strcpy(instr_bin_str + (sizeof(char) * strlen(instr_bin_str)),"000");  //sub-opcode
+	if (parse_reg(registers[2], instr_bin_str) == 5) {
+		printf("error4: parse_add() failed because parse_reg failed.\n");
+		return 4;
+	}
+	return 0;
+}
+
+int parse_sub(char* instr, char* instr_bin_str ) {
+    char registers[3];
+	char* token; 
+    char r;
+	int i = 0;
+	const char space_delim[2] = " ";
+	const char null_delim[1] = "";
+	const char comma_delim[2] = ", ";
+	token = strtok(instr, space_delim);          //skip command
+    instr = instr + strlen(token) + 1;           //go to next token
+    token = strtok(NULL, comma_delim);
+	while (token != NULL) {
+        sscanf(token, "R%c", &r);
+		registers[i] = '\0' + r;
+		if (i == 2) {
+			token = strtok(NULL, null_delim);
+		} else{
+			token = strtok(NULL, comma_delim);
+		}
+		i++;
+	}
+    memcpy(instr_bin_str, "0001", 5);  //opcode
+	if (parse_reg(registers[0], instr_bin_str) == 5) {
+		printf("error4: parse_sub() failed because parse_reg failed.\n");
+		return 4;
+	}
+	if (parse_reg(registers[1], instr_bin_str) == 5) {
+		printf("error4: parse_sub() failed because parse_reg failed.\n");
+		return 4;
+	}
+	strcpy(instr_bin_str + (sizeof(char) * strlen(instr_bin_str)),"010"); //sub-opcode
+	if (parse_reg(registers[2], instr_bin_str) == 5) {
+		printf("error4: parse_sub() failed because parse_reg failed.\n");
+		return 4;
+	}
+	return 0;
+}
+
+int parse_mul(char* instr, char* instr_bin_str ) {
+    char registers[3];
+	char* token; 
+    char r;
+	int i = 0;
+	const char space_delim[2] = " ";
+	const char null_delim[1] = "";
+	const char comma_delim[2] = ", ";
+	token = strtok(instr, space_delim);          //skip command
+    instr = instr + strlen(token) + 1;           //go to next token
+    token = strtok(NULL, comma_delim);
+	while (token != NULL) {
+        sscanf(token, "R%c", &r);
+		registers[i] = '\0' + r;
+		if (i == 2) {
+			token = strtok(NULL, null_delim);
+		} else{
+			token = strtok(NULL, comma_delim);
+		}
+		i++;
+	}
+    memcpy(instr_bin_str, "0001", 5); //opcode
+	if (parse_reg(registers[0], instr_bin_str) == 5) {
+		printf("error4: parse_mul() failed because parse_reg failed.\n");
+		return 4;
+	}
+	if (parse_reg(registers[1], instr_bin_str) == 5) {
+		printf("error4: parse_mul() failed because parse_reg failed.\n");
+		return 4;
+	}
+	strcpy(instr_bin_str + (sizeof(char) * strlen(instr_bin_str)),"001"); //sub-opcode
+	if (parse_reg(registers[2], instr_bin_str) == 5) {
+		printf("error4: parse_mul() failed because parse_reg failed.\n");
+		return 4;
+	}
+	return 0;
+}
+
+int parse_div(char* instr, char* instr_bin_str ) {
+    char registers[3];
+	char* token; 
+    char r;
+	int i = 0;
+	const char space_delim[2] = " ";
+	const char null_delim[1] = "";
+	const char comma_delim[2] = ", ";
+	token = strtok(instr, space_delim);          //skip command
+    instr = instr + strlen(token) + 1;           //go to next token
+    token = strtok(NULL, comma_delim);
+	while (token != NULL) {
+        sscanf(token, "R%c", &r);
+		registers[i] = '\0' + r;
+		if (i == 2) {
+			token = strtok(NULL, null_delim);
+		} else{
+			token = strtok(NULL, comma_delim);
+		}
+		i++;
+	}
+    memcpy(instr_bin_str, "0001", 5); //opcode
+	if (parse_reg(registers[0], instr_bin_str) == 5) {
+		printf("error4: parse_div() failed because parse_reg failed.\n");
+		return 4;
+	}
+	if (parse_reg(registers[1], instr_bin_str) == 5) {
+		printf("error4: parse_div() failed because parse_reg failed.\n");
+		return 4;
+	}
+	strcpy(instr_bin_str + (sizeof(char) * strlen(instr_bin_str)),"011"); //sub-opcode
+	if (parse_reg(registers[2], instr_bin_str) == 5) {
+		printf("error4: parse_div() failed because parse_reg failed.\n");
+		return 4;
+	}
+	return 0;
+}
+
+int parse_and(char* instr, char* instr_bin_str ) {
+    char registers[3];
+	char* token; 
+    char r;
+	int i = 0;
+	const char space_delim[2] = " ";
+	const char null_delim[1] = "";
+	const char comma_delim[2] = ", ";
+	token = strtok(instr, space_delim);          //skip command
+    instr = instr + strlen(token) + 1;           //go to next token
+    token = strtok(NULL, comma_delim);
+	while (token != NULL) {
+        sscanf(token, "R%c", &r);
+		registers[i] = '\0' + r;
+		if (i == 2) {
+			token = strtok(NULL, null_delim);
+		} else{
+			token = strtok(NULL, comma_delim);
+		}
+		i++;
+	}
+    memcpy(instr_bin_str, "0101", 5); //opcode
+	if (parse_reg(registers[0], instr_bin_str) == 5) {
+		printf("error4: parse_and() failed because parse_reg failed.\n");
+		return 4;
+	}
+	if (parse_reg(registers[1], instr_bin_str) == 5) {
+		printf("error4: parse_and() failed because parse_reg failed.\n");
+		return 4;
+	}
+	strcpy(instr_bin_str + (sizeof(char) * strlen(instr_bin_str)),"000"); //sub-opcode
+	if (parse_reg(registers[2], instr_bin_str) == 5) {
+		printf("error4: parse_and() failed because parse_reg failed.\n");
+		return 4;
+	}
+	return 0;
+}
+
+int parse_or(char* instr, char* instr_bin_str ) {
+    char registers[3];
+	char* token; 
+    char r;
+	int i = 0;
+	const char space_delim[2] = " ";
+	const char null_delim[1] = "";
+	const char comma_delim[2] = ", ";
+	token = strtok(instr, space_delim);          //skip command
+    instr = instr + strlen(token) + 1;           //go to next token
+    token = strtok(NULL, comma_delim);
+	while (token != NULL) {
+        sscanf(token, "R%c", &r);
+		registers[i] = '\0' + r;
+		if (i == 2) {
+			token = strtok(NULL, null_delim);
+		} else{
+			token = strtok(NULL, comma_delim);
+		}
+		i++;
+	}
+    memcpy(instr_bin_str, "0101", 5); //opcode
+	if (parse_reg(registers[0], instr_bin_str) == 5) {
+		printf("error4: parse_or() failed because parse_reg failed.\n");
+		return 4;
+	}
+	if (parse_reg(registers[1], instr_bin_str) == 5) {
+		printf("error4: parse_or() failed because parse_reg failed.\n");
+		return 4;
+	}
+	strcpy(instr_bin_str + (sizeof(char) * strlen(instr_bin_str)),"010"); //sub-opcode
+	if (parse_reg(registers[2], instr_bin_str) == 5) {
+		printf("error4: parse_or() failed because parse_reg failed.\n");
+		return 4;
+	}
+	return 0;
+}
+
+int parse_xor(char* instr, char* instr_bin_str ) {
+    char registers[3];
+	char* token; 
+    char r;
+	int i = 0;
+	const char space_delim[2] = " ";
+	const char null_delim[1] = "";
+	const char comma_delim[2] = ", ";
+	token = strtok(instr, space_delim);          //skip command
+    instr = instr + strlen(token) + 1;           //go to next token
+    token = strtok(NULL, comma_delim);
+	while (token != NULL) {
+        sscanf(token, "R%c", &r);
+		registers[i] = '\0' + r;
+		if (i == 2) {
+			token = strtok(NULL, null_delim);
+		} else{
+			token = strtok(NULL, comma_delim);
+		}
+		i++;
+	}
+    memcpy(instr_bin_str, "0101", 5); //opcode
+	if (parse_reg(registers[0], instr_bin_str) == 5) {
+		printf("error4: parse_xor() failed because parse_reg failed.\n");
+		return 4;
+	}
+	if (parse_reg(registers[1], instr_bin_str) == 5) {
+		printf("error4: parse_xor() failed because parse_reg failed.\n");
+		return 4;
+	}
+	strcpy(instr_bin_str + (sizeof(char) * strlen(instr_bin_str)),"011"); //sub-opcode
+	if (parse_reg(registers[2], instr_bin_str) == 5) {
+		printf("error4: parse_xor() failed because parse_reg failed.\n");
+		return 4;
+	}
+	return 0;
+}
+
+unsigned short int str_to_bin (char* instr_bin_str) {
+    int i;
+    for(i = 0; i < strlen(instr_bin_str); i++){
+        
+    }
+	return 0;
+}
+
+int power(int base, int exponent){
+    int s = 1;
+    while(exponent != 0) {
+        s = s*base;
+        exponent--;
+    }
+	return s;
+}
+
+int write_obj_file(char* filename, unsigned short int program_bin[ROWS] ) {
+	return 0;
+}
---
+++ assembler.c
@@ -0,0 +1,61 @@
+/***************************************************************************
+ * file name   : assembler.c                                               *
+ * author      : tjf & you                                                 *
+ * description : This program will assemble a .ASM file into a .OBJ file   *
+ *               This program will use the "asm_parser" library to achieve *
+ *			     its functionality.										   * 
+ *                                                                         *
+ ***************************************************************************
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "asm_parser.h"
+
+int main(int argc, char** argv) {
+    
+	char* filename = NULL ;					// name of ASM file
+	char  program [ROWS][COLS]; 			// ASM file line-by-line
+	char  program_bin_str [ROWS][17] ; 		// instructions converted to a binary string
+	unsigned short int program_bin [ROWS] ; // instructions in binary (HEX)
+    int i;
+    
+    if (argc != 2) {
+      printf("error1: usage: ./assembler <assembly_file.asm>\n");
+      return 0;
+    }
+    
+    //clean up memory
+    for (i = 0; i < ROWS; i++) {
+        memset(program[i], '\0', COLS);
+        memset(program_bin_str[i], '\0', 17);
+    }
+    memset(program_bin, '\0', ROWS);
+	
+    filename = argv[1];
+    if (read_asm_file(filename, program) == 2) {
+        printf("error2: read_asm_file() failed");
+        return 0;
+    }
+    
+    //reaching here means we can parse instructions out of program
+    
+    for (int i = 0; i < ROWS; i++) {
+        if (program[i][0] == '\0') {
+			continue;
+		}
+		parse_instruction(program[i], program_bin_str[i]);
+		program_bin[i] = str_to_bin(program_bin_str[i]);
+ 		//printf("%s\n", program[i]);
+    }
+    for (int i = 0; i < ROWS; i++) {
+        if (program_bin_str[i][0] != '\0') {
+            printf("%s\n", program_bin_str[i]);
+        }
+    }
+
+	//if (write_obj_file(filename, program_bin));
+    return 0;
+}
